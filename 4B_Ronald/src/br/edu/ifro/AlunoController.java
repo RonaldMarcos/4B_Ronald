83503691718ff55e1693181005c6e426eb92cb8a
04097711202 <04097711202@JIPA-LABIN04-11.ifro.local>
2018-05-09 07:59:30 -0400

    comclusao

--- 4B_Ronald/src/br/edu/ifro/AlunoController.java
+++ 4B_Ronald/src/br/edu/ifro/AlunoController.java
@@ -1,17 +1,26 @@
 package br.edu.ifro;
 
+import br.edu.ifro.Modelo.aluno;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class AlunoController implements Initializable {
 
     @FXML
     private JFXTextField txtNome;
+    @FXML
+    private JFXButton btnSalvar;
+    @FXML
+    private JFXButton btnCancelar;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -20,11 +29,21 @@ public class AlunoController implements Initializable {
 
     @FXML
     private void Salvar(ActionEvent event) {
-        EntityManagerFactory 
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("projetosistemaDS");
+            EntityManager em = emf.createEntityManager();
+
+            aluno c = new aluno();
+            c.setNome(txtNome.getText());
+            
+            em.getTransaction().begin();
+            em.persist(c);
+            em.getTransaction().commit();
     }
 
     @FXML
     private void Fechar(ActionEvent event) {
+        Stage stage = (Stage) btnCancelar.getScene().getWindow();
+        stage.close();
     }
     
 }
